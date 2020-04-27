22bbb705bb181892a57ad5028469ab776e306a0a
ArnaldoSena <arnaldoufcg@gmail.com>
2020-04-26 23:54:17 -0300

    Sugestao de troca

--- BC-06withMCU.ino
+++ BC-06withMCU.ino
@@ -30,6 +30,20 @@ void comando(int p1, int p2, int p3, int p4) {
     analogWrite(motorPin4, p4);
     delay(300);
   }
+/*
+  switch (var) {
+  case 1:
+    //do something when var equals 1
+    break;
+  case 2:
+    //do something when var equals 2
+    break;
+  default:
+    // if nothing else matches, do the default
+    // default is optional
+    break;
+}
+ */
 void loop(){
   if(bluetooth.available()){
        dadoBluetooth = bluetooth.read();
