6b32e3985229c347b4a31951eba006d6ab1a9ab3
ArnaldoSena <arnaldoufcg@gmail.com>
2020-05-02 22:36:29 -0300

    Ajuste no controle de velocidade

--- BC-06withMCU.ino
+++ BC-06withMCU.ino
@@ -13,7 +13,11 @@ const int EN_B = 5;
 const int motorPin3 = 7;
 const int motorPin4 = 6; 
 //speed
-int motor_speed;
+int motor_speed = 0;
+int p1 = LOW;
+int p2 = LOW;
+int p3 = LOW;
+int p4 = LOW;
 SoftwareSerial bluetooth(pinoRX, pinoTX); //PINOS QUE EMULAM A SERIAL, ONDE
 //O PINO 2 É O RX E O PINO 3 É O TX
  
@@ -29,48 +33,54 @@ void setup(){
   pinMode(EN_B,OUTPUT);
   pinMode(motorPin3,OUTPUT);
   pinMode(motorPin4,OUTPUT);
-  motor_speed = 60;
-  comando(LOW,LOW,LOW,LOW, motor_speed);
+  comando(p1,p2,p3,p4,motor_speed);
 }
-void comando(int p1, int p2, int p3, int p4, int ms) {
-    digitalWrite(motorPin1, p1);
-    digitalWrite(motorPin2, p2);
-    digitalWrite(motorPin3, p3);
-    digitalWrite(motorPin4, p4);
-    analogWrite(EN_A, ms);
-    analogWrite(EN_B, ms);
+void comando(int r1, int r2, int r3, int r4, int velocity) {
+    digitalWrite(motorPin1, r1);
+    digitalWrite(motorPin2, r2);
+    digitalWrite(motorPin3, r3);
+    digitalWrite(motorPin4, r4);
+    analogWrite(EN_A, velocity);
+    analogWrite(EN_B, velocity);
     delay(300);
-  }
+}
 void loop(){
   if(bluetooth.available()){
       dadoBluetooth = bluetooth.read();
       if(motor_speed > 220) motor_speed = 220;
-      if(motor_speed < 60) motor_speed = 60;
+      if(motor_speed < 50) motor_speed = 50;
       switch (dadoBluetooth) {
         case 'u':
-          comando(LOW,HIGH,HIGH,LOW, motor_speed);
+          p1 = LOW; p2 = HIGH; p3 = HIGH; p4 = LOW;  
+          comando(p1,p2,p3,p4,motor_speed);
         break;
         case 'r':
-          comando(HIGH,LOW,HIGH,LOW, motor_speed);
+          p1 = HIGH; p2 = LOW; p3 = HIGH; p4 = LOW;  
+          comando(p1,p2,p3,p4,motor_speed);
         break;
         case 'l':
-          comando(LOW,HIGH,LOW,HIGH, motor_speed);
+          p1 = LOW; p2 = HIGH; p3 = LOW; p4 = HIGH;  
+          comando(p1,p2,p3,p4, motor_speed);
         break;
         case 'd':
-          comando(HIGH,LOW,LOW,HIGH, motor_speed);          
+          p1 = HIGH; p2 = LOW; p3 = LOW; p4 = HIGH;  
+          comando(p1,p2,p3,p4,motor_speed);          
         break;
         case '3'://#
-          comando(LOW,LOW,LOW,LOW, motor_speed);
-          motor_speed = 60; 
+          p1 = LOW; p2 = LOW; p3 = LOW; p4 = LOW;  
+          comando(p1,p2,p3,p4,motor_speed);
+          motor_speed = 0; 
         break;
         case '1': //x
-          motor_speed = motor_speed + 20;
+          motor_speed = motor_speed + 10;
+          comando(p1,p2,p3,p4,motor_speed);
         break;
         case '2': //o
           //do something when var equals 2
         break;
         case '4'://tri
-          motor_speed = motor_speed - 20;
+          motor_speed = motor_speed - 10;
+          comando(p1,p2,p3,p4,motor_speed);
         break;
         case 'M'://F1
         //do something when var equals 2
@@ -90,7 +100,7 @@ void loop(){
  //       case '0':
         //do something when var equals 2
 //        break;
-      default:
+        default:
         // if nothing else matches, do the default
         // default is optional
         break;
